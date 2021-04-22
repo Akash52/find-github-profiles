3dfdcc4a98ca2d9ff0ddac8d06c507bf87b56f7c
Akash52 <ac8572611@gmail.com>
2021-04-22 19:02:10 +0530

    start react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Layout/Navbar'
 import Seaction from './components/Layout/Seaction'
 import Search from './components/Layout/Users/Search'
+import Useritem from './components/Layout/Users/Useritem'
+import Users from './components/Layout/Users/Users'
 
 const App = () => {
   return (
@@ -12,123 +14,7 @@ const App = () => {
           <Seaction />
           <Search />
 
-          <div className="p-3 flex flex-wrap mt-10">
-            <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4">
-              <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
-                <div className="px-6">
-                  <img
-                    alt="..."
-                    src="https://avatars.githubusercontent.com/u/1?v=4"
-                    className="shadow-xl rounded-full max-w-full mx-auto  hover:opacity-80 transition-opacity duration-500 ease-in"
-                    style={{ maxWidth: '150px' }}
-                  />
-                  <div className="pt-6 text-center">
-                    <h5 className="text-xl font-bold">Romina Hadid</h5>
-                    <div className="mt-6">
-                      <button
-                        className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
-                        type="button"
-                      >
-                        More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4">
-              <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
-                <div className="px-6">
-                  <img
-                    alt="..."
-                    src="https://avatars.githubusercontent.com/u/1?v=4"
-                    className="shadow-xl rounded-full max-w-full mx-auto transition hover:opacity-80 transition-opacity duration-500 ease-in"
-                    style={{ maxWidth: '150px' }}
-                  />
-                  <div className="pt-6 text-center">
-                    <h5 className="text-xl font-bold">Romina Hadid</h5>
-                    <div className="mt-6">
-                      <button
-                        className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
-                        type="button"
-                      >
-                        More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4">
-              <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
-                <div className="px-6">
-                  <img
-                    alt="..."
-                    src="https://avatars.githubusercontent.com/u/1?v=4"
-                    className="shadow-xl rounded-full max-w-full mx-auto transition hover:opacity-80 transition-opacity duration-500 ease-in"
-                    style={{ maxWidth: '150px' }}
-                  />
-                  <div className="pt-6 text-center">
-                    <h5 className="text-xl font-bold">Romina Hadid</h5>
-                    <div className="mt-6">
-                      <button
-                        className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
-                        type="button"
-                      >
-                        More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4">
-              <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
-                <div className="px-6">
-                  <img
-                    alt="..."
-                    src="https://avatars.githubusercontent.com/u/1?v=4"
-                    className="shadow-xl rounded-full max-w-full mx-auto transition hover:opacity-80 transition-opacity duration-500 ease-in"
-                    style={{ maxWidth: '150px' }}
-                  />
-                  <div className="pt-6 text-center">
-                    <h5 className="text-xl font-bold">Romina Hadid</h5>
-                    <div className="mt-6">
-                      <button
-                        className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
-                        type="button"
-                      >
-                        More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4">
-              <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
-                <div className="px-6">
-                  <img
-                    alt="..."
-                    src="https://avatars.githubusercontent.com/u/1?v=4"
-                    className="shadow-xl rounded-full max-w-full mx-auto transition hover:opacity-80 transition-opacity duration-500 ease-in"
-                    style={{ maxWidth: '150px' }}
-                  />
-                  <div className="pt-6 text-center">
-                    <h5 className="text-xl font-bold">Romina Hadid</h5>
-                    <div className="mt-6">
-                      <button
-                        className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
-                        type="button"
-                      >
-                        More
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Users />
         </div>
       </section>
     </main>
---
+++ src/components/Layout/Users/Useritem.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+
+class Useritem extends Component {
+  state = {
+    id: '1',
+    login: 'mojombo',
+    avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
+  }
+
+  render() {
+    const { login, avatar_url, html_url } = this.state
+    return (
+      <div className="w-full p-5 md:w-6/12 lg:w-4/12 lg:mb-0 mb-12 px-4 transform transition duration-500 hover:scale-110">
+        <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-5">
+          <div className="px-6">
+            <img
+              alt="..."
+              src={avatar_url}
+              className="shadow-xl rounded-full max-w-full mx-auto  hover:opacity-80 transition-opacity duration-500 ease-in"
+              style={{ maxWidth: '150px' }}
+            />
+            <div className="pt-6 text-center">
+              <h5 className="text-xl font-bold">{login}</h5>
+              <div className="mt-6">
+                <button
+                  className="text-white font-bold uppercase text-base px-8 py-3 rounded-full shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1  transition-all   duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ..."
+                  type="button"
+                >
+                  <a href={html_url}>More</a>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default Useritem
